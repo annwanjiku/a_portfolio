b228867894f8b98060e9bb81cc30b020365d184b
ann wanjiku <wanjikunganga21@gmail.com>
2023-11-04 18:59:30 +0300

    Making changes to my personal website

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 if(typeof document!='undefined')
  {
 
+document.body.addEventListener('click', function(event) {
+    var dropdownContent = document.getElementById("contact-options");
+    if (!event.target.matches('.dropbtn') && dropdownContent.classList.contains('show')) {
+        dropdownContent.classList.remove('show');
+    }
+});
+
 function showContactOptions() {
     var dropdownContent = document.getElementById("contact-options");
     dropdownContent.classList.toggle("show");
@@ -21,19 +28,37 @@ default:
 }
 }
 
+let aboutClickCount = 0;
+
 function showAbout() {
+    aboutClickCount++;
     document.getElementById("about-section").style.display = "block";
     document.getElementById("entire-page").style.display = "none";
+    document.getElementById("myphotocontainer").style.display = 'block';
+    document.documentElement.classList.add("about-clicked");
+
+    if (aboutClickCount > 2) {
+        alert("Please click the home button to return to the homepage.");
+        aboutClickCount = 0;
+       
+    }
+    
 }
+
 
 
 function showHomePage() {
     document.getElementById("about-section").style.display = "block";
     document.getElementById("entire-page").style.display = "block";
+    document.getElementById("myphotocontainer").style.display='none';
+
+    document.documentElement.classList.remove("about-clicked");
+
+
     
     setTimeout(function() {
         alert("Welcome to the home page!");
     }, 1000); 
 }
 
- }
+}
